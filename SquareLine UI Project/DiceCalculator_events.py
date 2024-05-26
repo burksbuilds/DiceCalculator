e612244719f3052ace9257cb892f742fcd859330
Andrew Burks <92557438+BurksEngineering@users.noreply.github.com>
2024-05-26 10:07:41 -0400

    Added SquareLine LVGL project

---
+++ SquareLine UI Project/DiceCalculator_events.py
@@ -0,0 +1,40 @@
+
+def reset_roll(event_struct):
+    return
+
+
+def sum_clicked(event_struct):
+    return
+
+
+def rolled_clicked(event_struct):
+    return
+
+
+def d4_clicked(event_struct):
+    return
+
+
+def d6_clicked(event_struct):
+    return
+
+
+def d8_clicked(event_struct):
+    return
+
+
+def d10_clicked(event_struct):
+    return
+
+
+def d12_clicked(event_struct):
+    return
+
+
+def d20_clicked(event_struct):
+    return
+
+
+def rolled_click(event_struct):
+    return
+
